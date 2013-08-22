de91f519a25da6ec591983cd69d751811d786848
Patrick Hulin <phulin@mit.edu>
2013-08-22 08:32:59 -0400

    Write more efficient Ord instance for Identifier

--- src/Data/LLVM/Types/Identifiers.hs
+++ src/Data/LLVM/Types/Identifiers.hs
@@ -15,6 +15,7 @@ module Data.LLVM.Types.Identifiers (
 import Control.DeepSeq
 import Data.Hashable
 import Data.Text ( Text, unpack, pack )
+import Data.Monoid ( (<>) )
 
 data Identifier = LocalIdentifier { _identifierContent :: !Text
                                   , _identifierHash :: !Int
@@ -26,13 +27,29 @@ data Identifier = LocalIdentifier { _identifierContent :: !Text
                 | MetaIdentifier { _identifierContent :: !Text
                                  , _identifierHash :: !Int
                                  }
-                  deriving (Eq, Ord)
+                  deriving (Eq)
 
 instance Show Identifier where
   show LocalIdentifier { _identifierContent = t } = '%' : unpack t
   show AnonymousLocalIdentifier { _identifierNumber = n } = '%' : show n
   show GlobalIdentifier { _identifierContent = t } = '@' : unpack t
   show MetaIdentifier { _identifierContent = t } = '!' : unpack t
+
+-- More efficient than default instance
+instance Ord Identifier where
+  LocalIdentifier cont1 hash1 `compare` LocalIdentifier cont2 hash2
+    = (hash1 `compare` hash2) <> (cont1 `compare` cont2)
+  LocalIdentifier{} `compare` _ = LT
+  _ `compare` LocalIdentifier{} = GT
+  GlobalIdentifier cont1 hash1 `compare` GlobalIdentifier cont2 hash2
+    = (hash1 `compare` hash2) <> (cont1 `compare` cont2)
+  GlobalIdentifier{} `compare` _ = LT
+  _ `compare` GlobalIdentifier{} = GT
+  MetaIdentifier cont1 hash1 `compare` MetaIdentifier cont2 hash2
+    = (hash1 `compare` hash2) <> (cont1 `compare` cont2)
+  MetaIdentifier{} `compare` _ = LT
+  _ `compare` MetaIdentifier{} = GT
+  AnonymousLocalIdentifier n1 `compare` AnonymousLocalIdentifier n2 = n1 `compare` n2
 
 instance Hashable Identifier where
   hashWithSalt s (AnonymousLocalIdentifier n) = s `hashWithSalt` n
